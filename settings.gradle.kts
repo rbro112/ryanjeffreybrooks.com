78a78daf6da7a49d8f9f629e2edcfe8ed5f5a825
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2021-06-30 11:05:23 -0700

    Remove webpack config and resolution strategy for unused lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                //Workaround, because kotlin-dce-js plugin not available by id on gradlePluginPortal
-                "kotlin-dce-js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
 }
 
 rootProject.name = "personal_site"
--- webpack.config.d/css.js
+++
@@ -1,6 +0,0 @@
-config.resolve.modules.push("processedResources/js/main");
-
-config.module.rules.push({
-    test: /\.css$/,
-    use: ['style-loader', 'css-loader']
-});
